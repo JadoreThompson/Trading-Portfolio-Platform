ecf6432ccd05500fcb8a1617fec9188e0741fbec
LucioJamieAI <148231849+JadoreThompson@users.noreply.github.com>
2024-10-20 11:00:36 +0100

    landing page scaffold

--- toolbox/urls.py
+++ toolbox/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('pages.urls')),
     path('accounts/', include('accounts.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('admin/', admin.site.urls),
